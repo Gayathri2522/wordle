532cc6aa7da9e56141383c5f5698fd588a0f3a60
S Gayathri <BL.EN.U4CSE20143@BL.STUDENTS.AMRITA.EDU>
2024-02-13 17:43:48 +0530

    Final Result

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,17 +2,46 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import WonBanner from '../WonBanner/WonBanner';
+import LostBanner from '../LostBanner/LostBanner';
 
 import GuessInput from '../GuessInput/GuessInput';
-
+import GuessResults from '../GuessResults/GuessResults';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <GuessInput />
-    ;
+  const [gameStatus, setGameStatus] = React.useState('running');
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess){
+    //creating a brand new arrray because setGuessses is a stale array and hence the extra delay in won or lost
+    const nextGUESSES = [...guesses, tentativeGuess]
+    setGuesses(nextGUESSES);
+
+    if(tentativeGuess.toUpperCase() === answer){
+      setGameStatus("won");
+    }
+      else if(nextGUESSES.length >= NUM_OF_GUESSES_ALLOWED){
+        setGameStatus("lost");
+      }
+  }
+  return (
+    <>
+    {gameStatus}
+    <GuessResults guesses={guesses}  answer ={answer}/>
+    <GuessInput gameStatus= {gameStatus} handleSubmitGuess={ handleSubmitGuess }  />
+    {gameStatus === 'won' && (
+      <WonBanner numOfGuesses={guesses.length} />
+    )}
+    {gameStatus === 'lost' && 
+      <LostBanner answer ={answer} />
+    }
+    </>
+  );
 }
 
 export default Game;
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-function GuessInput() {
-  const [guess, setGuess] = React.useState('');
+function GuessInput({gameStatus, handleSubmitGuess}) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
 
   function handleSubmit(event){
     event.preventDefault();
     
-    if(guess.length!== 5){
-      window.alert("Please enter 5 characters.")
+    if(tentativeGuess.length!== 5){
+      window.alert("Please enter 5 characters💖")
     }
 
-    console.log({guess});
+    handleSubmitGuess(tentativeGuess);
 
-    setGuess('');
+    setTentativeGuess('');
   }
 
 
   return <div>
   <form onSubmit={handleSubmit} className="guess-input-wrapper">
   <label htmlFor="guess-input">Enter guess:</label>
-  <input value={guess} onChange={(event) =>{
+  <input
+  disabled = {gameStatus != "running"}
+  value={tentativeGuess} onChange={(event) =>{
     const nextGuess = event.target.value.toUpperCase();
-    setGuess(nextGuess);
+    setTentativeGuess(nextGuess);
   }}  id="guess-input" type="text" maxLength={5} minLength={5} />
 </form>
   </div>;
